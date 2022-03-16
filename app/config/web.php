fec1a9f44b62d035e299ff90891b3a5cfffc2ec2
sham <sham@csgo.com>
2022-03-16 22:28:41 +0300

    init

---
+++ app/config/web.php
@@ -0,0 +1,116 @@
+<?php
+
+use yii\debug\Module;
+use yii\log\FileTarget;
+use yii\swiftmailer\Mailer;
+use app\models\User;
+use yii\caching\FileCache;
+use app\modules\GraphQL;
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'modules'=> [
+        'api' => [
+            'class' => GraphQL::class,
+            //graphql config
+            'schema' => [
+                'query' => [
+                    'user' => 'app\graphql\query\UsersQuery'
+                ],
+                'mutation' => [
+                    'login'
+                ],
+            // you do not need to set the types if your query contains interfaces or fragments
+            // the key must same as your defined class
+                'types' => [
+                ],
+            ],
+        ],
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'aXfN0qsmozTk_WFVtfYikfv9htmXBcff',
+        ],
+        'cache' => [
+            'class' => FileCache::class,
+        ],
+        'user' => [
+            'identityClass' => User::class,
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => Mailer::class,
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+//        'graphql' => [
+//            'class' => 'path\to\components',
+//                    //graphql config
+//            'schema' => [
+//                'query' => [
+//                    'user' => 'app\graphql\query\UsersQuery'
+//                ],
+//                'mutation' => [
+//                    'login'
+//                ],
+//                // you do not need to set the types if your query contains interfaces or fragments
+//                // the key must same as your defined class
+//                'types'=>[
+//                ],
+//            ],
+//        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => Module::class,
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => \yii\gii\Module::class,
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
